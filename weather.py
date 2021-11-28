848f5c0d8724cbd67444ca24d2f8ff68f64ef420
Biraj <29871174+birajstha@users.noreply.github.com>
2021-11-28 18:15:03 -0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,22 +1,20 @@
-# -*- coding: utf-8 -*-
-
-# Python program to find current
-# weather details of any city
-# using openweathermap api
- 
-# import required modules
+import serial
+from gtts import gTTS
+import subprocess
+from datetime import date, datetime
 import requests, json
- 
+
+
+
 # Enter your API key here
-api_key = "a4bdd4b3b747843ede0c9f2fd5a5c3f5"
- 
+api_key = "a4bdd4b3b747843ede0c9f2fd5a5c3f5" 
 # base_url variable to store url
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
  
 # Give city name
-#city_name = input("Enter city name : ")
- 
 
+#city_name = input("Enter city name : ")
+city_name = "Morrisville"
 
 # http://api.openweathermap.org/data/2.5/weather?appid=a4bdd4b3b747843ede0c9f2fd5a5c3f5&q=morrisville
 
@@ -26,7 +24,7 @@ base_url = "http://api.openweathermap.org/data/2.5/weather?"
 # complete url address
 #  base_url + "appid=" + api_key + "&q=" + city_name
 
-complete_url = "http://api.openweathermap.org/data/2.5/weather?appid=a4bdd4b3b747843ede0c9f2fd5a5c3f5&q=morrisville"
+complete_url = "http://api.openweathermap.org/data/2.5/weather?appid=a4bdd4b3b747843ede0c9f2fd5a5c3f5&q=">
  
 # get method of requests module
 # return response object
@@ -41,42 +39,56 @@ x = response.json()
 # Check the value of "cod" key is equal to
 # "404", means city is found otherwise,
 # city is not found
-if x["cod"] != "404":
- 
-    # store the value of "main"
-    # key in variable y
-    y = x["main"]
- 
-    # store the value corresponding
-    # to the "temp" key of y
-    current_temperature = y["temp"]
- 
-    # store the value corresponding
-    # to the "pressure" key of y
-    current_pressure = y["pressure"]
- 
-    # store the value corresponding
-    # to the "humidity" key of y
-    current_humidity = y["humidity"]
- 
-    # store the value of "weather"
-    # key in variable z
-    z = x["weather"]
- 
-    # store the value corresponding
-    # to the "description" key at
-    # the 0th index of z
-    weather_description = z[0]["description"]
- 
-    # print following values
-    print(" Temp : " +
-                    str(current_temperature) +
-          "\n Atm Press : " +
-                    str(current_pressure) +
-          "\n Humidity : " +
-                    str(current_humidity) +
-          "\n Description : " +
-                    str(weather_description))
- 
+
+
+
+if x["cod"] != "404": 
+        # store the value of "main"
+        # key in variable y
+        y = x["main"]
+        # store the value corresponding
+        # to the "temp" key of y
+        current_temperature = y["temp"]
+        # store the value corresponding
+        # to the "pressure" key of y
+        current_pressure = y["pressure"]
+        # store the value corresponding
+        # to the "humidity" key of y
+        current_humidity = y["humidity"]
+        # store the value of "weather"
+        # key in variable z
+        z = x["weather"]
+        # store the value corresponding
+        # to the "description" key at
+        # the 0th index of z
+        weather_description = z[0]["description"]
+        today = date.today()
+        today = today.strftime("%B %d, %Y")
+        now = datetime.now()
+        now = now.strftime("%H:%M")
+        greet = "Good Morning Honey. Hope you had a good sleep. Today is " + today + "."
+        say_time = "It is " + now + "now."
+        #configure the serial connection
+        ser = serial.Serial(port='/dev/serial0',baudrate=19200)
+        if ser.isOpen():
+                ser.write(str.encode(chr(12))) # 12 is clear and reset cursor
+                ser.write(str.encode("City : " + city_name))
+                ser.write(str.encode(chr(13))) # 13 Starts a new line
+                temp = round((current_temperature-273.15)*1.8 + 32, 2)
+                ser.write(str.encode("Temp : " + str(temp) + "F"))
+                ser.write(str.encode(chr(13))) # 13 Starts a new line
+                ser.write(str.encode("P/H : " + str(current_pressure) +" & "+ str(current_humidity)))
+                ser.write(str.encode(chr(13))) # 13 Starts a new line
+                ser.write(str.encode(str(weather_description)))
+
 else:
-    print(" City Not Found ")
+        exit()
+
+
+myText = greet + say_time + ". There is " + str(weather_description) + "in" + city_name + " area. The Tem>
+language = 'en'
+output = gTTS(text=myText, lang=language, slow=False)
+output.save("output.mp3")
+subprocess.run(['mpg321', r'output.mp3'])
+
+      
